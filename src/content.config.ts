02b7854a5a306aba8fa648fcba5d150cf48d4233
wakkobr <gpessine@yahoo.com>
2026-08-16 23:35:34 -0300

    Atualizando content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,8 @@
-// src/content.config.ts
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const terminal = defineCollection({
-  loader: glob({ pattern: '*.{md,mdx}', base: './src/content/terminal' }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: 'src/content/terminal' }),
   schema: z.object({
     title: z.string(),
     summary: z.string(),
